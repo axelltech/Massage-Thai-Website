30ba8543acaa7c58d904a92691677449847ed9c1
axelltech <axelll.tech@gmail.com>
2023-10-11 21:33:44 +0300

    Other fixes

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,30 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobileMenu = document.querySelector('.header .nav-bar .nav-list ul');
 const header = document.querySelector('.header.content');
 const headerNav = document.querySelector('.nav-bar');
+const cookieBox = document.querySelector(".wrapper");
+const buttons = document.querySelectorAll(".button");
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   mobileMenu.classList.toggle('active');
 })
+
+const executeCodes = () => {
+  if(document.cookie.includes("codingPosii")) return;
+  cookieBox.classList.add("show");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+      if(button.id == "acceptBtn") {
+        document.cookie = "cookieBy= codingPosii; max-age=" + 60 * 60 * 24 * 30;
+      } else {
+      }
+    })
+  })
+}
+
+window.addEventListener("load", executeCodes);
+
 
 document.addEventListener('scroll', () => {
   let scroll_position = window.scrollY;
@@ -20,10 +39,15 @@ document.addEventListener('scroll', () => {
   }
 })
 
+
+
+
+
+
 //jQuery to switch logic and place
-$(document).ready(function() {
-    $.fancybox.defaults.hash = false;
-  });
-const options = {
-    Hash: false,
-  };
+// $(document).ready(function() {
+//     $.fancybox.defaults.hash = false;
+//   });
+// const options = {
+//     Hash: false,
+//   };
