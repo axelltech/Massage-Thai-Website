e1ec0427cfa870980e9f85122285d24c6caa49d0
axelltech <axelll.tech@gmail.com>
2023-10-11 22:48:25 +0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  // site: 'https://axelltech.github.io',
-  // base: '/Massage-Thai-Website',
+  site: 'https://axelltech.github.io',
+  base: '/Massage-Thai-Website',
   // https://github.com/axelltech/Massage-Thai-Website
   integrations: [
     tailwind({
