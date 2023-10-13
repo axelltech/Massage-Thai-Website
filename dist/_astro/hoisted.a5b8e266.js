1d14a87e12917ace90172d99308759d9a8aa1172
axelltech <axelll.tech@gmail.com>
2023-10-13 14:12:44 +0300

    Dist

---
+++ dist/_astro/hoisted.a5b8e266.js
@@ -0,0 +1,1 @@
+import"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js";import"https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js";import"./hoisted.91ebaa74.js";
